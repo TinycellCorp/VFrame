b456d72c69ccfd4615c0c20044048da61cf801f8
aaron <qkrsogusl3@gmail.com>
2022-01-11 11:44:10 +0900

    ILazyVariantComponentPool.Clear 중복 구현 수정

---
+++ Assets/VFrame/Runtime/Pool/ComponentPool/ResolveLazyComponentPoolBase.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using UnityEngine;
+using VContainer;
+using VFrame.Extension;
+using VFrame.Pool.ComponentPool.UniRx.Toolkit;
+
+namespace VFrame.Pool.ComponentPool
+{
+    
+    public interface ILazyComponentPoolInitializable
+    {
+        void Initialize(GameObject prefab);
+    }
+
+    public abstract class ResolveLazyComponentPoolBase<T> : ObjectPool<T>, ILazyComponentPoolInitializable
+        where T : Component
+    {
+        private readonly IObjectResolver _resolver;
+        private readonly Transform _parent;
+        public bool IsInitialize { get; private set; } = false;
+        private GameObject _prefab;
+        private IDisposable _beforeHandler;
+
+        private GameObject Prefab
+        {
+            get
+            {
+                if (!IsInitialize) throw new Exception($"not initialized lazy pool: [{typeof(T).Name}]");
+
+                return _prefab;
+            }
+        }
+
+        protected ResolveLazyComponentPoolBase(IObjectResolver resolver, Transform parent)
+        {
+            _resolver = resolver;
+            _parent = parent;
+        }
+
+        public void Initialize(GameObject prefab)
+        {
+            if (IsInitialize) throw new Exception($"initialized lazy pool: {typeof(T).Name}");
+            IsInitialize = true;
+            _prefab = prefab;
+        }
+
+        protected sealed override T CreateInstance()
+        {
+            _beforeHandler = Prefab.InstantiateBefore(out T component, _parent);
+            _resolver.Inject(component);
+            return component;
+        }
+
+        protected override void OnBeforeRent(T instance)
+        {
+            this.ChangeParent(instance, _parent);
+        }
+
+        protected override void OnBeforeReturn(T instance)
+        {
+            base.OnBeforeReturn(instance);
+            this.ChangeParent(instance, _parent);
+        }
+
+
+        protected void EndBuild(T instance)
+        {
+            if (_beforeHandler == null)
+            {
+                instance.gameObject.SetActive(true);
+            }
+            else
+            {
+                _beforeHandler.Dispose();
+                _beforeHandler = null;
+            }
+        }
+
+
+        protected ParameterBuilder Build(out T component)
+        {
+            component = Rent();
+            return new ParameterBuilder(this, component);
+        }
+
+        protected readonly struct ParameterBuilder : IDisposable
+        {
+            private readonly ResolveLazyComponentPoolBase<T> _pool;
+            private readonly T _instance;
+
+            public ParameterBuilder(ResolveLazyComponentPoolBase<T> pool, T instance)
+            {
+                _pool = pool;
+                _instance = instance;
+            }
+
+            public void Dispose()
+            {
+                _pool.EndBuild(_instance);
+            }
+        }
+    }
+}
--- Assets/VFrame/Runtime/Pool/ComponentPool/ResolveLazyVariantComponentPool.cs
+++ Assets/VFrame/Runtime/Pool/ComponentPool/ResolveLazyVariantComponentPool.cs
@@ -5,74 +5,7 @@ using VContainer;
 
 namespace VFrame.Pool.ComponentPool
 {
-    public interface ILazyVariantComponentPoolInitializable
-    {
-        void Initialize(Action<Dictionary<string, GameObject>> configuration);
-    }
-
-    public abstract class ResolveLazyVariantComponentPoolBase<TPool, T> : ILazyVariantComponentPoolInitializable
-        where TPool : ResolveLazyComponentPoolBase<T>
-        where T : Component
-    {
-        private readonly Dictionary<string, GameObject> _prefabs = new Dictionary<string, GameObject>();
-        private readonly Dictionary<string, TPool> _pools = new Dictionary<string, TPool>();
-
-        private readonly IObjectResolver _resolver;
-        private readonly Transform _parent;
-
-        public bool IsInitialize { get; private set; } = false;
-
-
-        protected ResolveLazyVariantComponentPoolBase(IObjectResolver resolver, Transform parent)
-        {
-            _resolver = resolver;
-            _parent = parent;
-        }
-
-        public void Initialize(Action<Dictionary<string, GameObject>> configuration)
-        {
-            if (IsInitialize) throw new Exception($"initialized lazy variant pool: {typeof(T).Name}");
-
-            configuration(_prefabs);
-            foreach (var pair in _prefabs)
-            {
-                var pool = CreatePool(_resolver, _parent);
-                pool.Initialize(pair.Value);
-                _pools.Add(pair.Key, pool);
-            }
-
-            IsInitialize = true;
-        }
-
-        public T Rent() => throw new Exception("require using key");
-
-        public void Return(T instance) => throw new Exception("require using key");
-
-        public T Rent(string key)
-        {
-            var pool = GetPool(key);
-            return pool.Rent();
-        }
-
-        public void Return(string key, T component)
-        {
-            var pool = GetPool(key);
-            pool.Return(component);
-        }
-
-        protected abstract TPool CreatePool(IObjectResolver resolver, Transform parent);
-
-        protected TPool GetPool(string key)
-        {
-            if (_pools.TryGetValue(key, out var pool))
-            {
-                return pool;
-            }
-
-            throw new KeyNotFoundException($"{typeof(T).Name} {key}");
-        }
-    }
-
+   
     public class ResolveLazyVariantComponentPool<T> :
         ResolveLazyVariantComponentPoolBase<ResolveLazyComponentPool<T>, T>,
         ILazyVariantComponentPool<T>
@@ -87,11 +20,6 @@ namespace VFrame.Pool.ComponentPool
             return new ResolveLazyComponentPool<T>(resolver, parent);
         }
 
-        public void Clear(string key)
-        {
-            var pool = GetPool(key);
-            pool.Clear();
-        }
     }
 
     public class ResolveLazyVariantComponentPool<TParam, T> :
@@ -112,12 +40,6 @@ namespace VFrame.Pool.ComponentPool
         {
             var pool = GetPool(key);
             return pool.Rent(param);
-        }
-
-        public void Clear(string key)
-        {
-            var pool = GetPool(key);
-            pool.Clear();
         }
     }
 }
