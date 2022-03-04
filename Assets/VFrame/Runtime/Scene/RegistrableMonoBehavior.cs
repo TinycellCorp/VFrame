18ad26678d0b7e825ae6896f4203dbcde5e8ec27
aaron <qkrsogusl3@gmail.com>
2022-03-04 17:58:30 +0900

    RegistrableMonoBehavior 추가

---
+++ Assets/VFrame/Runtime/Scene/RegistrableMonoBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace VFrame.Scene
+{
+    [DefaultExecutionOrder(-5002)]
+    public class RegistrableMonoBehavior<TComponent> : MonoBehaviour, SceneScope.IReserveRegistrable
+        where TComponent : MonoBehaviour
+    {
+        public string SceneName => gameObject.scene.name;
+
+        public void Configure(IContainerBuilder builder)
+        {
+            builder.RegisterComponent<TComponent>(this as TComponent);
+        }
+
+        protected virtual void Awake()
+        {
+            SceneScope.Reserve(this);
+        }
+    }
+}
--- Assets/VFrame/Runtime/Scene/SceneScope.cs
+++ Assets/VFrame/Runtime/Scene/SceneScope.cs
@@ -1,16 +1,57 @@
-﻿using VFrame.UI;
-using VFrame.UI.Group;
+﻿using System.Collections.Generic;
+using System.Linq;
+using VFrame.UI;
 using VContainer;
 using VContainer.Unity;
 
 namespace VFrame.Scene
 {
+    public static class SceneScope
+    {
+        private static readonly Dictionary<string, Queue<IReserveRegistrable>> Installers =
+            new Dictionary<string, Queue<IReserveRegistrable>>();
+
+
+        public static void Reserve(IReserveRegistrable registrable)
+        {
+            if (!Installers.TryGetValue(registrable.SceneName, out var queue))
+            {
+                queue = new Queue<IReserveRegistrable>();
+                Installers.Add(registrable.SceneName, queue);
+            }
+
+            queue.Enqueue(registrable);
+        }
+
+        public static void Configure(LifetimeScope scope, IContainerBuilder builder)
+        {
+            var sceneName = scope.gameObject.scene.name;
+            if (Installers.TryGetValue(sceneName, out var queue))
+            {
+                while (queue.Any())
+                {
+                    queue.Dequeue().Configure(builder);
+                }
+
+                Installers.Remove(sceneName);
+            }
+        }
+
+        public interface IReserveRegistrable
+        {
+            string SceneName { get; }
+            void Configure(IContainerBuilder builder);
+        }
+    }
+
+
     public class SceneScope<TEntry> : LifetimeScope
         where TEntry : SceneEntry
     {
         protected override void Configure(IContainerBuilder builder)
         {
             base.Configure(builder);
+            SceneScope.Configure(this, builder);
             UISystem.Configure(this, builder);
             builder.RegisterEntryPoint<TEntry>().AsSelf();
             builder.Register<AssetCache>(Lifetime.Singleton);
@@ -32,6 +73,7 @@ namespace VFrame.Scene
         protected override void Configure(IContainerBuilder builder)
         {
             base.Configure(builder);
+            SceneScope.Configure(this, builder);
             UISystem.Configure(this, builder);
             builder.RegisterEntryPoint<TEntry>().AsSelf();
             builder.Register<AssetCache>(Lifetime.Singleton);
