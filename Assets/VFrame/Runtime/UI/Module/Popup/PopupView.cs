6d02adb79daad72fb612f8b26c68bd9aa8253fda
aaron <qkrsogusl3@gmail.com>
2022-03-11 17:05:38 +0900

    PopupView Layer등록 제거

--- Assets/VFrame/Runtime/UI/Module/Popup/PopupView.cs
+++ Assets/VFrame/Runtime/UI/Module/Popup/PopupView.cs
@@ -1,4 +1,5 @@
 ﻿using VFrame.UI.Animation;
+using VFrame.UI.Group;
 using VFrame.UI.View;
 
 namespace VFrame.UI.Module.Popup
@@ -11,9 +12,29 @@ namespace VFrame.UI.Module.Popup
     {
         protected override void Awake()
         {
-            UISystem.Register<TView, PopupLayer, IPopupGroup, IAnimation<IPopupView>>(this);
+            UISystem.RegisterPopup<TView, IPopupGroup, IAnimation<IPopupView>>(this);
+            //Layer 스펙 점검 필요. 그 전 까지 비활성화 (AddressableView 부모 못 찾는 상태) 
+            // UISystem.Register<TView, PopupLayer,IPopupGroup, IAnimation<IPopupView>>(this);
             Init();
         }
+    }
+}
 
+namespace VFrame.UI
+{
+    public partial class UISystem
+    {
+        public static void RegisterPopup<TView, TGroup, TAnimation>(ComponentView<TView> view)
+            where TView : IView
+            where TGroup : class, IGroup
+            where TAnimation : class, IAnimation
+        {
+            RegisterView(view);
+            ExecuteCacheCommand(new MergeCommand(
+                new CacheAnimationCommand<TView, TAnimation>(view),
+                new CacheTransitionCommand<TView>(view),
+                new CacheGroupCommand<TView, TGroup>(view)
+            ));
+        }
     }
 }
